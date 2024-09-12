7131f60d36644776e113f2daf554bd3c1c216bd9
Sam Merwan <sam.mjalaoui@gmail.com>
2024-09-12 16:24:29 +0200

    update ChatList to show availability

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -1,6 +1,10 @@
 import { List, ListItem, ListItemText, Divider } from '@mui/material';
+import CircleIcon from '@mui/icons-material/Circle';
+import { useTheme } from '@mui/material/styles';
 
 const ChatList = ({ users, onSelectUser, selectedUserId }) => {
+    const theme = useTheme();
+    
     return (
         <List>
             {users.map((user) => (
@@ -9,8 +13,12 @@ const ChatList = ({ users, onSelectUser, selectedUserId }) => {
                         button
                         selected={user._id === selectedUserId}
                         onClick={() => onSelectUser(user)}
+                        sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
                     >
                         <ListItemText primary={user.username} />
+                        <CircleIcon
+                            sx={{ color: user.isAvailable ? theme.palette.success.main : theme.palette.error.main }}
+                        />
                     </ListItem>
                     <Divider />
                 </div>
