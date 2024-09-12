d8d4ac6f93384e89e498aadfa65cf474b92034ab
Sam Merwan <sam.mjalaoui@gmail.com>
2024-09-12 16:02:10 +0200

    update walkDetails to display time of creation

--- src/components/WalkDetails.jsx
+++ src/components/WalkDetails.jsx
@@ -1,6 +1,17 @@
 import { Card, CardContent, Typography } from '@mui/material';
 
 const WalkDetails = ({ walk, onClick }) => {
+  const isSamePoint = walk.startingPoint === walk.endPoint;
+
+  const formattedDate = new Date(walk.createdAt).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  const formattedStartingPoint = walk.startingPoint.split(',')[0];
+
   return (
     <Card 
       onClick={() => onClick(walk)}
@@ -8,13 +19,17 @@ const WalkDetails = ({ walk, onClick }) => {
     >
       <CardContent>
         <Typography variant="h5" component="div">
-          Walk from {walk.startingPoint} to {walk.endPoint}
+          Walk from {formattedStartingPoint} 
+          {!isSamePoint && ` to ${walk.endPoint}`}
         </Typography>
         <Typography variant="body2">
           Duration: {walk.durationInMinutes} minutes
         </Typography>
         <Typography variant="body2">
-          Created by: {walk.user.name}
+          Created on: {formattedDate}
+        </Typography>
+        <Typography variant="body2">
+          By: {walk.user.name}
         </Typography>
       </CardContent>
     </Card>
